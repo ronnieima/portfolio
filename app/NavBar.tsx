1609f5c884138aab73b2eedf43075ef7bc09fc93
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-17 11:51:22 +0100

    start styling navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NavBar = () => {
-  return <nav className="h-[64px] bg-indigo-500">NavBar</nav>;
+  return (
+    <nav className="h-[64px] bg-blue-800/90 p-2 flex justify-between items-center">
+      <h2 className="text-neutral-200 text-2xl">今川.dev</h2>
+    </nav>
+  );
 };
 
 export default NavBar;
