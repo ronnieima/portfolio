d31b1a8b11bb19db7103711dfb8c915d1611c8fa
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-17 11:39:28 +0100

    initial commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <nav className="h-[64px] bg-indigo-500">NavBar</nav>;
+};
+
+export default NavBar;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-background min-h-screen p-8">
+      <h1 className="text-6xl">こんにちは</h1>
+    </div>
+  );
+}
