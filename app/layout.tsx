ead9b228dad996fafde81dae663789e5b5870d7b
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-17 19:55:56 +0100

    add theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import { Providers } from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <>
+        <Providers>
           <NavBar />
           {children}
-        </>
+        </Providers>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import ThemeSwitch from "./ThemeSwitch";
 
 export default function Home() {
   return (
-    <main className="bg-neutral-800 min-h-screen text-background p-8">
+    <main className="dark:bg-neutral-800 dark:text-zinc-300  bg-slate-200 text-foreground min-h-screen  p-8">
       <h1 className="text-6xl ">こんにちは</h1>
-      <Button
+      <ThemeSwitch />
     </main>
   );
 }
