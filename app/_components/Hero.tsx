112587028dc5cbbf415ffc4662bd0efe112e9933
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-27 12:07:55 +0100

    tweak grid

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,7 +5,7 @@ import Greeting from "./ui/Greeting";
 
 function Hero() {
   return (
-    <section className="flex flex-col gap-y-16 sm:grid sm:grid-cols-[1fr_1fr_1fr_1fr] sm:grid-rows-[1fr_2fr_1fr] font-semibold h-full max-w-screen-md mx-auto py-8 px-16 sm:px-4">
+    <section className="flex flex-col gap-y-16 sm:grid sm:grid-cols-[1fr_1.5fr_1.5fr_1fr] sm:grid-rows-[1fr_2fr_1fr] font-semibold h-full max-w-screen-md mx-auto py-8 px-16 sm:px-4">
       <Greeting />
       <Kanji />
       <HeroSubtitle />
