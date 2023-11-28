076de2a4443b885ab48132a686d190a756883545
Ronnie <ronnie.imagawa@gmail.com>
2023-11-28 12:08:32 +0100

    fix widths

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,7 +5,7 @@ import Greeting from "./ui/Greeting";
 
 function Hero() {
   return (
-    <section className="flex flex-col gap-y-8 sm:grid sm:grid-cols-[1fr_6fr_6fr_1fr] sm:grid-rows-[1fr_2fr_1fr] font-semibold h-full max-w-screen-md mx-auto py-8 px-6 lg:px-8 sm:px-4">
+    <section className="flex flex-col gap-y-8 sm:grid sm:grid-cols-[1fr_6fr_6fr_1fr] sm:grid-rows-[1fr_2fr_1fr] font-semibold h-full max-w-screen-lg mx-auto py-8 px-6 lg:px-8 sm:px-4">
       <Greeting />
       <Kanji />
       <HeroSubtitle />
