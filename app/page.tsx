2df55d288296bdc1fd630208bac585c642a898f4
Ronnie <ronnie.imagawa@gmail.com>
2023-11-28 09:34:27 +0100

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import Footer from "./_components/Footer";
-import Link from "next/link";
 import Hero from "./_components/Hero";
-import { PropsWithChildren, ReactNode } from "react";
 
 export default function Home() {
   return (
