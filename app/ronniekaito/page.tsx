90a906b621afc6547ec27aabf1761f8c166d0af3
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-29 11:32:32 +0100

    add small arrow and fix tablet view

--- app/ronniekaito/page.tsx
+++ app/ronniekaito/page.tsx
@@ -4,15 +4,28 @@ import { notoSerif } from '@/utils/fonts';
 
 function AboutMePage() {
   return (
-    <main className="mx-auto flex max-w-screen-xl animate-fade-down grid-cols-[0.5fr_1fr_1fr_0.5fr] flex-col justify-center gap-8 px-8 text-center animate-duration-1000 sm:grid sm:gap-0 sm:gap-x-2 sm:text-left">
-      <div className="pointer-events-none relative col-start-2 self-center justify-self-end text-2xl ">
-        <p className="text-center tracking-widest">ABOUT ME</p>
+    <main className="mx-auto flex max-w-screen-xl grid-cols-[0.5fr_1fr_1fr_0.5fr] flex-col justify-center gap-8 px-8 text-center md:grid md:gap-0 md:gap-x-2 md:text-left">
+      <div className="pointer-events-none relative col-start-2 flex animate-fade-left flex-col items-center self-center justify-self-end animate-duration-1000">
+        <header className="relative">
+          <p
+            className={`text-2xl tracking-widest ${notoSerif.className} mb-4 w-fit`}
+          >
+            ABOUT ME
+          </p>
+          <Image
+            src="/arrow.svg"
+            alt="arrow"
+            width={40}
+            height={40}
+            className="absolute -left-12 right-24 top-8 -rotate-12 invert-0 dark:invert"
+          />
+        </header>
         <Image
           src="/headshot.png"
           alt="My Headshot Photo"
           width={200}
           height={200}
-          className=" rounded-full sm:w-full"
+          className=" mx-auto rounded-full sm:w-4/5"
         />
         <Image
           src="/Flag_of_Guam.svg"
@@ -22,20 +35,20 @@ function AboutMePage() {
           className="absolute bottom-0 right-0 w-2/5"
         />
       </div>
-      <header className="flex flex-col items-center justify-center gap-1 sm:items-start">
-        <h1 className={`order-2 text-5xl sm:text-7xl${notoSerif.className}`}>
+      <header className="flex animate-fade-right flex-col items-center justify-center gap-2 animate-duration-1000 md:items-start">
+        <h1 className={`order-2 text-5xl sm:text-5xl ${notoSerif.className}`}>
           Ronnie Kaito <br />
-          Imagawa.
+          Imagawa,
         </h1>
-        <h2 className="order-1 text-lg sm:text-3xl">
+        <h2 className="order-1 text-lg sm:text-2xl">
           it&apos;s nice to meet you, i&apos;m
         </h2>
         <h3 className="order-3 text-lg sm:text-3xl">
-          full stack web developer
+          full stack web developer.
         </h3>
       </header>
 
-      <p className="col-span-2 col-start-2 row-start-2 mb-16 self-start text-lg sm:mb-0 sm:text-3xl">
+      <p className="col-span-2 col-start-2 row-start-2 mb-16 animate-fade-down self-start text-lg animate-duration-1000 sm:mb-0 sm:text-lg">
         I am an active duty Air Force IT technician stationed in Ramstein,
         Germany with 3 years of experience. As a recent Computer Science
         graduate born and raised from the beautiful, tiny island of Guam, I love
