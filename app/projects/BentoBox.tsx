b23b248e47c53bc351d0f224ae5b69e785874b28
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-30 10:18:41 +0100

    add bento boxes

---
+++ app/projects/BentoBox.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+type BentoBoxProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+function BentoBox({ children, className }: BentoBoxProps) {
+  return (
+    <div
+      className={` rounded-3xl bg-[#1E1E1E] p-8 text-foreground ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default BentoBox;
