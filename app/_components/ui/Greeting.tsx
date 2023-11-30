6e0fc787feea4207ad069cc12fd2f6feb2927316
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-11-30 15:17:02 +0100

    fix text size

--- app/_components/ui/Greeting.tsx
+++ app/_components/ui/Greeting.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Greeting() {
   return (
     <div className=" col-span-2 flex animate-fade-right flex-col justify-end animate-duration-[2000ms]">
-      <h1 className={`text-4xl  ${notoSerif.className}`}>
+      <h1 className={`text-2xl  ${notoSerif.className}`}>
         hello, my name is <br />
         <span className="text-5xl text-primary"> Ronnie Kaito</span>.
       </h1>
