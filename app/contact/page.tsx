6f29096dc909a1cab108858aa7e4d20651793c7a
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-03 13:50:29 +0100

    fix contact page mobile layout

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactHeader from './ContactHeader';
 
 function ContactPage() {
   return (
-    <main className="mx-auto max-h-[700px] max-w-xl animate-fade-down gap-x-8 px-8 py-8 animate-duration-1000">
+    <main className="mx-auto max-w-xl animate-fade-down gap-x-8 p-8 animate-duration-1000">
       <ContactHeader />
       <ContactCard />
     </main>
