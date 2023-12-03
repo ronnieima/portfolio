4de9dee9a4d02b98e9a491311ee2ff3f843dcd0b
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-03 13:44:30 +0100

    tweak spacing

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import ProjectsBentoBoxes from './ProjectsBentoBoxes';
 
 function ProjectsPage() {
   return (
-    <main className=" my-8 flex flex-col items-center justify-center">
+    <main className=" my-8 flex flex-col items-center justify-center gap-8">
       <h1 className={`${notoSerif.className} animate-fade-down text-6xl`}>
         My projects.
       </h1>
