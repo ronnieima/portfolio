311d719203a3c98549431f6efa98157d9ebcf3b4
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-12-03 08:31:05 +0100

    add fade down animation

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import ContactHeader from './ContactHeader';
 
 function ContactPage() {
   return (
-    <main className="mx-auto min-h-[100svh] max-w-xl gap-x-8 px-8 ">
+    <main className="mx-auto max-h-[700px] max-w-xl animate-fade-down gap-x-8 px-8 py-8 animate-duration-1000">
       <ContactHeader />
       <ContactCard />
     </main>
