a72f476ade044c90bc9b34f958b17af860505215
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-01-01 11:38:19 +0100

    change from email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
     const body = await request.json();
     const { name, email, message } = body;
     const data = await resend.emails.send({
-      from: 'Darius <dariusboldy@resend.dev>',
+      from: 'imagawa.dev <ronniekaito@imagawa.dev>',
       to: 'ronnie.imagawa@gmail.com',
       subject: `Contact message from imagawa.dev : ${name} - ${email}`,
       react: EmailTemplate({
