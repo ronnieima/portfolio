767a8851dbbb780deb847af6949c87c5c5009b81
Kaito <70875687+ronnieima@users.noreply.github.com>
2025-08-03 12:54:01 -0400

    feat: add max width to navbar

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -4,27 +4,30 @@ import MobileMenu from './MobileMenu';
 import Crest from './ui/Crest';
 import NavLinks from './ui/NavLinks';
 import SocialLinks from './ui/SocialLinks';
+import MaxWidthWrapper from './MaxWidthContainer';
 
 function NavBar() {
   return (
     <nav className="absolute top-0 z-10 w-full py-2.5">
-      <div className="flex w-full  items-center justify-between gap-10 bg-transparent px-4 ">
-        <Link href="/" className="flex items-center gap-4">
-          <Crest size={30} />
-          <h2>ronnie kaito imagawa.</h2>
-        </Link>
+      <MaxWidthWrapper>
+        <div className="flex w-full  items-center justify-between gap-10 bg-transparent xl:px-4 ">
+          <Link href="/" className="flex items-center gap-4">
+            <Crest size={30} />
+            <h2>ronnie kaito imagawa.</h2>
+          </Link>
 
-        <ul className="hidden md:gap-5 lg:flex xl:gap-16">
-          <NavLinks />
-        </ul>
-
-        <div className="flex gap-3 align-middle">
-          <ul className="hidden gap-2 lg:flex">
-            <SocialLinks />
+          <ul className="hidden md:gap-5 lg:flex xl:gap-16">
+            <NavLinks />
           </ul>
-          <MobileMenu />
+
+          <div className="flex gap-3 align-middle">
+            <ul className="hidden gap-2 lg:flex">
+              <SocialLinks />
+            </ul>
+            <MobileMenu />
+          </div>
         </div>
-      </div>
+      </MaxWidthWrapper>
     </nav>
   );
 }
